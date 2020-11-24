f417f4e906a513ef60766e8d6c41f3f908bc24ed
gensense <yellowchemcom@gmail.com>
2020-11-25 00:09:52 +0900

    Updated

--- src/components/CV/index.js
+++ src/components/CV/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Layout } from 'antd'
 import Header from './Header'
 import Sidebar from '../Sidebar'
-{//import TopTech from './TopTech'}
-{//import SkillList from './SkillList'}
-{//import Experience from './Experience'}
-{//import PitchLine from './PitchLine'}
-{//import Volunteer from './Volunteer'}
-{//import Education from './Education'}
+{/*import TopTech from './TopTech'
+import SkillList from './SkillList'
+import Experience from './Experience'
+import PitchLine from './PitchLine'
+import Volunteer from './Volunteer'
+import Education from './Education'
+import Recommendations from './Recommendations'*/}
 import Experiments from './Experiments'
-{//import Recommendations from './Recommendations'}
 import Footer from './Footer'
 import data from './data.json'
 
@@ -22,13 +22,13 @@ const CV = () => (
       {data.experiments && (
         <Experiments experiments={data.experiments} tech={data.tech} />
       )}
-   {/*   {data.pitchLine && <PitchLine pitchLine={data.pitchLine} />}
+   {/*{data.pitchLine && <PitchLine pitchLine={data.pitchLine} />}
       {data.tech && data.topTech && <TopTech topTech={data.topTech} tech={data.tech} />}
       {data.skills && <SkillList skills={data.skills} />}
       {data.experience && <Experience experience={data.experience} />}
       {data.volunteer && <Volunteer volunteer={data.volunteer} />}
       {data.education && <Education education={data.education} />}
-      {data.recommendations && <Recommendations recommendations={data.recommendations} />} */}
+      {data.recommendations && <Recommendations recommendations={data.recommendations} />}*/}
       {data.footer && <Footer footer={data.footer} />}
     </Content>
   </>
