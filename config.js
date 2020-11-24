7133c7e91246ac2c3bae5772b803cb06257717ad
gensense <yellowchemcom@gmail.com>
2020-11-25 00:33:21 +0900

    Updated

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ module.exports = {
       path: '/'
     },
     {
-      label: 'Personal project',
+      label: 'CV',
       path: '/cv'
     }
   ],
--- src/components/CV/index.js
+++ src/components/CV/index.js
@@ -3,13 +3,13 @@ import { Layout } from 'antd'
 import Header from './Header'
 import Sidebar from '../Sidebar'
 {/*import TopTech from './TopTech'
-import SkillList from './SkillList'
 import Experience from './Experience'
 import PitchLine from './PitchLine'
 import Volunteer from './Volunteer'
 import Education from './Education'
 import Recommendations from './Recommendations'*/}
 import Experiments from './Experiments'
+import SkillList from './SkillList'
 import Footer from './Footer'
 import data from './data.json'
 
@@ -22,9 +22,9 @@ const CV = () => (
       {data.experiments && (
         <Experiments experiments={data.experiments} tech={data.tech} />
       )}
+      {data.skills && <SkillList skills={data.skills} />}
    {/*{data.pitchLine && <PitchLine pitchLine={data.pitchLine} />}
       {data.tech && data.topTech && <TopTech topTech={data.topTech} tech={data.tech} />}
-      {data.skills && <SkillList skills={data.skills} />}
       {data.experience && <Experience experience={data.experience} />}
       {data.volunteer && <Volunteer volunteer={data.volunteer} />}
       {data.education && <Education education={data.education} />}
