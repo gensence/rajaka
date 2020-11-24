2cf8212d472f644efe417c2f8dd0e9c279cf346a
gensense <yellowchemcom@gmail.com>
2020-11-24 21:27:32 +0900

    first commit

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   url: 'https://gifted-mcclintock-4f3509.netlify.app',
   pathPrefix: '/',
   title: 'Rajaka.net',
@@ -10,46 +9,25 @@ module.exports = {
   disqusShortname: 'Author',
   postsPerPage: 20,
   googleAnalyticsId: '',
-=======
-  url: 'https://gatsby-starter-blog-and-cv.netlify.app',
-  pathPrefix: '/',
-  title: 'Blog by Marat Dospolov',
-  keywords:
-    'account surfer, trello bookmark, trello, productivity, javascript, Marat Dospolov, choosy windows, browser selector',
-  subtitle: 'Developer. Optimizer. Learner.',
-  copyright: '© 2020 | Kyiv, Ukraine',
-  disqusShortname: 'dospolov',
-  postsPerPage: 20,
-  googleAnalyticsId: 'UA-6589522-7',
->>>>>>> origin/master
   menu: [
     {
       label: 'Blog',
       path: '/'
     },
     {
-<<<<<<< HEAD
       label: 'Personal project',
-=======
-      label: 'CV',
->>>>>>> origin/master
       path: '/cv'
     }
   ],
   author: {
-<<<<<<< HEAD
     name: 'Rajaka.net',
     photo: '/photo.jpg',
     bio: '',
     contacts: {
-=======
-    name: 'Marat Dospolov',
+    name: 'Rajaka.net',
     photo: '/photo.jpg',
     bio: 'Developer. Optimizer. Learner.',
     contacts: {
-      // don't remove fields, just make them empty string ''
-      // https://github.com/gatsbyjs/gatsby/issues/2392
->>>>>>> origin/master
       github: 'dospolov',
       twitter: 'marat_dospolov',
       linkedin: 'dospolov',
