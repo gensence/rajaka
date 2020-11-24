18a9882d9ffa077cc86cac5a65ce8793b0475d7d
gensense <yellowchemcom@gmail.com>
2020-11-25 01:23:15 +0900

    Updated

--- src/components/CV/SkillList.js
+++ src/components/CV/SkillList.js
@@ -5,23 +5,23 @@ const SkillList = ({ skills }) => (
   <div>
     <Row type="flex" justify="space-around">
       <Col span={11}>
-        <h3 className="text-center mt-10 mb-5">Soft skills</h3>
+        <h3 className="text-center mt-10 mb-5">Software</h3>
       </Col>
       <Col span={11} offset={1}>
-        <h3 className="text-center mt-10 mb-5">Hard skills</h3>
+        <h3 className="text-center mt-10 mb-5">Web</h3>
       </Col>
     </Row>
     <Card className="">
       <Row type="flex" justify="space-around">
         <Col span={11}>
           <div>
-            {skills.soft.featured.map(skill => (
+            {skills.software.featured.map(skill => (
               <Tag color="#722ed1" key={skill} className="mb-1 text-base">
                 {skill}
               </Tag>
             ))}
           </div>
-          {skills.soft.other.map(skill => (
+          {skills.software.other.map(skill => (
             <Tag color="purple" key={skill} className="mb-1 text-base">
               {skill}
             </Tag>
@@ -29,13 +29,13 @@ const SkillList = ({ skills }) => (
         </Col>
         <Col span={11} offset={1}>
           <div>
-            {skills.hard.featured.map(skill => (
+            {skills.web.featured.map(skill => (
               <Tag color="#1890ff" key={skill} className="mb-1 text-base">
                 {skill}
               </Tag>
             ))}
           </div>
-          {skills.hard.other.map(skill => (
+          {skills.web.other.map(skill => (
             <Tag color="blue" key={skill} className="mb-1 text-base">
               {skill}
             </Tag>
