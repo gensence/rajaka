a762503064bfb355cf7185035834573db7b0b739
gensense <yellowchemcom@gmail.com>
2020-11-24 21:19:17 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  url: 'https://gifted-mcclintock-4f3509.netlify.app',
+  pathPrefix: '/',
+  title: 'Rajaka.net',
+  keywords:
+    'account surfer, trello bookmark, trello, productivity, javascript, Marat Dospolov, choosy windows, browser selector',
+  subtitle: 'Developer. Optimizer. Learner.',
+  copyright: '© 2020 | Rajaka.net, Japan',
+  disqusShortname: 'Author',
+  postsPerPage: 20,
+  googleAnalyticsId: '',
+  menu: [
+    {
+      label: 'Blog',
+      path: '/'
+    },
+    {
+      label: 'Personal project',
+      path: '/cv'
+    }
+  ],
+  author: {
+    name: 'Rajaka.net',
+    photo: '/photo.jpg',
+    bio: '',
+    contacts: {
+      github: 'dospolov',
+      twitter: 'marat_dospolov',
+      linkedin: 'dospolov',
+      telegram: 'dospolov',
+      instagram: 'dospolov',
+      facebook: 'dospolov',
+      email: '',
+      rss: '/rss.xml'
+    }
+  }
+}
